71b914b6f8728604da3ac3f2d307132137cd5d66
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-01-26 14:48:00 +0300

    Update __main__.py

--- ArStringTep/__main__.py
+++ ArStringTep/__main__.py
@@ -23,4 +23,4 @@ async def anony_boot():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("Stopping String Gen Bot...")
+    LOGGER.info("Stopping String Bot...")
