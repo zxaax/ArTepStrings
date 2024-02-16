7d87e61853490a98adcf19c8fe918a15d0737861
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-02-16 14:04:34 +0300

    Update start.py

--- ArStringTep/modules/start.py
+++ ArStringTep/modules/start.py
@@ -9,7 +9,7 @@ from ArStringTep.utils import add_served_user, keyboard
 async def f_start(_, message: Message):
     await message.reply_text(
         text=f"✦ **مرحبًــا  👋** {message.from_user.first_name},\n\n  {Anony.mention},\n**✦ يمكنـك استخـدامي لـ استخـراج كـود الجلسـة لـ تنصيـب  تيبثـون ✨**\n **✦ ملاحظـات هامـة 🚨 : -** لا تشارك كود الجلسـة لأي أحد **، - عند وصول الكود لك ضع مسافة بين كل رقم ورقم مثـال : 1 2 3 4 5  .**",
-        reply_markup=keyboarأ
+        reply_markup=keyboard
         disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
